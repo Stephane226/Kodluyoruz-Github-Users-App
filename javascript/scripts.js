835cac7e973e542d1c68300c6dc0b3d8a1367b70
Masaaki Stephane <bstephane97@yandex.com>
2021-09-24 00:13:55 +0300

    Repositories UI

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,4 @@
 //Set the height of body content minimum to screen size
 var minimumHeight = window.innerHeight;
-var headerFooterHeights = 250;
+var headerFooterHeights = 200;
 document.getElementById("ms-body-id").style.minHeight = `${minimumHeight}` - headerFooterHeights + "px";
