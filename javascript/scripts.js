b36708bd6c082206ec4382604852a25e31d00100
Masaaki Stephane <bstephane97@yandex.com>
2021-09-23 19:42:46 +0300

    Differant Block Of The App
    Header, Body Content Ve Footer olarak uygulamamız 3 componente ayrırdık

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,1 +1,5 @@
-alert('hello')
+//Set the height of body content minimum to screen size
+var minimumHeight = window.innerHeight;
+alert(minimumHeight)
+var headerFooterHeights = 280;
+document.getElementById("ms-body-id").style.minHeight = `${minimumHeight}` - headerFooterHeights + "px";
