35f2ca682a08c6263d57c81264785bc9e74490de
Masaaki Stephane <bstephane97@yandex.com>
2021-09-24 20:28:39 +0300

    assigning general variables

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -18,8 +18,29 @@ async function fetchAPI() {
     const response = await fetch(urlFetch);
     
     if (response.ok) {
-      let json = await response.json();
-      console.log(json)
+      let results = await response.json();
+      console.log(results);
+
+ const userName = results.name;
+ const userLogin = results.login;
+ const avatar = results.avatar_url;
+ const profileUrl = results.html_url;
+ const location = results.location;
+ const public_repos = results.public_repos;
+ const followers = results.followers;
+ const following = results.following;
+ const repos_url = results.repos_url;
+
+
+
+
+
+
+
+
+    }
+    else{
+        alert("No User!")
     }
     } catch(e) {
      console.log(e);
