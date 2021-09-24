4f5069f9c1ab96fff229e944571178cf69db663b
Masaaki Stephane <bstephane97@yandex.com>
2021-09-24 07:15:31 +0300

    Ui updates

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,4 @@
 //Set the height of body content minimum to screen size
 var minimumHeight = window.innerHeight;
-var headerFooterHeights = 200;
+var headerFooterHeights = 280;
 document.getElementById("ms-body-id").style.minHeight = `${minimumHeight}` - headerFooterHeights + "px";
