17bc8f9ce965c5c4d189add5cf56eaa0dda68aa8
Masaaki Stephane <bstephane97@yandex.com>
2021-09-24 20:14:18 +0300

    Fetch Tries

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,3 +2,30 @@
 var minimumHeight = window.innerHeight;
 var headerFooterHeights = 280;
 document.getElementById("ms-body-id").style.minHeight = `${minimumHeight}` - headerFooterHeights + "px";
+
+
+
+//Fetch
+
+    let url = "https://api.github.com/users/";
+   const inputValue = document.getElementById("searchInput");
+async function fetchAPI() {
+    try {
+        
+        var urlFetch = url+inputValue.value;
+        console.log(urlFetch)
+
+    const response = await fetch(urlFetch);
+    
+    if (response.ok) {
+      let json = await response.json();
+      console.log(json)
+    }
+    } catch(e) {
+     console.log(e);
+    }
+   }
+document.getElementById("searchClick").addEventListener("click", function(){
+    return fetchAPI()
+
+})
