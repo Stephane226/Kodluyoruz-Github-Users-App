a28482acc091381c869bec93b3afee7ddcc18cf8
Masaaki Stephane <bstephane97@yandex.com>
2021-09-25 15:04:58 +0300

    Some Litle  Fetch problems

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -3,23 +3,47 @@ var minimumHeight = window.innerHeight;
 var headerFooterHeights = 280;
 document.getElementById("ms-body-id").style.minHeight = `${minimumHeight}` - headerFooterHeights + "px";
 
+//HTML variables declaration 
+/*
+const pseudo = document.getElementById("pseudo");
+const userName = document.getElementById("userName");
+const avatar = document.getElementById("avatar");
+const location = document.getElementById("location");
+const followers = document.getElementById("followers");
+const following = document.getElementById("following");
+const public_repo = document.getElementById("public_repo");
+
+
+
+
+
+
+
+
+ 
+*/
+
+
+
 
 
 //Fetch
 
+const repoSide = document.getElementById("repositoriesLand");
+console.log(repoSide)
     let url = "https://api.github.com/users/";
    const inputValue = document.getElementById("searchInput");
 async function fetchAPI() {
     try {
         
         var urlFetch = url+inputValue.value;
-        console.log(urlFetch)
+  
 
     const response = await fetch(urlFetch);
     
     if (response.ok) {
       let results = await response.json();
-      console.log(results);
+  
 
  const userName = results.name;
  const userLogin = results.login;
@@ -30,9 +54,95 @@ async function fetchAPI() {
  const followers = results.followers;
  const following = results.following;
  const repos_url = results.repos_url;
+ const bio = results.bio;
+ 
 
 
 
+
+
+
+document.getElementById("drop-header-cnt").innerHTML = `
+
+<div class="ms-body-right-cnt">
+<div class="ms-body-right-top">
+    <img class="user-porfil-pic" id="avatar" src="${avatar}"  alt="User_profile">
+</div>
+<div class="ms-body-right-top right-top-2">
+  <div> <div class="title-info"> <i class="fas fa-user"></i><span>PSEUDO :</span></div><span id="pseudo">${userLogin}</span></div> 
+  <div> <div class="title-info"> <i class="fas fa-meteor"></i><span>USER NAME :</span></div><span id="userName">${userName}</span></div> 
+
+  <div> <div class="title-info"> <i class="fas fa-project-diagram"></i><span>Public Repositories :</span></div><span id="public_repo">${public_repos}</span></div> 
+
+</div>
+
+
+<div class="ms-body-right-top right-top-3">
+    <div> <div class="title-info"> <i class="fas fa-user"></i><span>Country :</span></div><span id="location">${location}</span></div> 
+    <div> <div class="title-info"> <i class="fas fa-meteor"></i><span>followers:</span></div><span id="followers">${followers}</span></div> 
+
+    <div> <div class="title-info"> <i class="fas fa-project-diagram"></i><span>following :</span></div><span id="following">${following}</span></div> 
+</div>
+
+
+<div class="ms-body-right-top right-top-4">
+    <div> <div class="title-info"> <i class="fas fa-user"></i><span>Bio :</span></div><span>${bio}</span></div> 
+    <div> <div class="title-info"> <i class="fas fa-meteor"></i><span>USER NAME :</span></div><span>Stephane226</span></div> 
+
+    <div> <div class="title-info"> <i class="fas fa-project-diagram"></i><span>USER NAME :</span></div><span>Stephane226</span></div> 
+</div>
+
+</div>
+
+
+
+`;
+
+
+
+fetch(repos_url)
+  .then(responseRepo => responseRepo.json)
+  .then(commits => {
+
+    repoSide.innerHTML = commits.map(data =>{
+         
+
+       
+        console.log(data);
+const repoName =data.name;
+console.log(repoName)
+const allow_forking = data.allow_forking;
+
+   
+const contributors_url = data.contributors_url;
+const dateCreated = data.created_at;
+const description = data.description;
+const languagesUsed = data.languages_url;
+const mainLanguage = data.language;
+const liscence = data.license.name;
+
+return `
+        
+<div class="ms-repo">
+<div class="ms-repo-header">
+   <h3>${repoName}</h3>
+</div>
+<div class="ms-body-right-top right-top-3 ms-repo-header-cnt">
+<div> <div class="title-info-repo"> <i class="fas fa-project-diagram"></i><span>Description :</span></div><span>${description}</span></div> 
+    <div> <div class="title-info-repo"> <i class="fas fa-user"></i><span>Languages </span>:<span>${mainLanguage}</span></div> 
+    <div> <div class="title-info-repo"> <i class="fas fa-meteor"></i><span>Contributors :</span></div><span></span> </div> 
+    <div> <div class="title-info-repo"> <i class="fas fa-project-diagram"></i><span>Liscence :</span></div><span>${liscence}</span></div> 
+    <div> <div class="title-info-repo"> <i class="fas fa-project-diagram"></i><span>Fork Allow :</span></div><span>${allow_forking}</span></div> 
+</div>
+</div>
+
+
+`
+ 
+    
+    })
+    
+})
 
 
 
@@ -46,7 +156,12 @@ async function fetchAPI() {
      console.log(e);
     }
    }
+
+  
 document.getElementById("searchClick").addEventListener("click", function(){
-    return fetchAPI()
+    return fetchAPI();
+    
 
 })
+
+
